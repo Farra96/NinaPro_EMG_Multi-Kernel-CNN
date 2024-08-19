fa816704b1b7e24cad5744a5760d231e39a9cbfb
Fratti Riccardo <101409111+Farra96@users.noreply.github.com>
2024-08-19 14:09:46 +0200

    Merged_Databases_Grid_Search.py

--- Merged_Databases_Grid_Search.py
+++ Merged_Databases_Grid_Search.py
@@ -1,83 +1,87 @@
+#%%
 import csv
 import random
 import sys
 import os
-
-from sklearn import metrics
-
-os.chdir('/home/ricardo/')
-
 import numpy as np  # numpy library
 import torch
 import torch.nn as nn
 import pandas as pd
 import argparse
 
+from sklearn import metrics
 from scipy.stats import loguniform
+
+os.chdir('/home/riccardo/')
+sys.path.append('./MKCNN')
 
 # CUSTOM LYBRARIES
 from DATALOADERS import Pandas_Dataset
 from Training_type import random_GS
 from Data_Processing_Utils import Norm_each_sub_by_own_param, train_val_test_split_df
 
-#%% Delete FutureWarning
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)  # -> if pandas is bothering you
 
-# %% Loading and merging datasets
-# Choose which window to use (so which dataframe to load)
-wnd_len = 300
 
-# DB2            -> Left handed already deleted at creation
-df_DB2 = pd.read_csv(f'/home/ricardo/DB2/Dataframe/dataframe_wnd_{wnd_len}_filt_20_500.csv',
-                     dtype={'sub': np.int8, 'label': np.int8, 'rep': np.int8, 'sample_index': np.uint32})
+# #%% Delete FutureWarning
+# import warnings
+# warnings.simplefilter(action='ignore', category=FutureWarning)  # -> if pandas is bothering you
 
-# DB7
-df_DB7 = pd.read_csv(f'/home/ricardo/DB7/Dataframe/dataframe_wnd_{wnd_len}_filt_20_500.csv',
-                     dtype={'sub': np.int8, 'label': np.int8, 'rep': np.int8, 'sample_index': np.uint32})
-############ DROPPING LEFT HANDED ######################
-left_hand_DB7 = [1, 4, 5, 14]
-df_DB7.drop(df_DB7[df_DB7['sub'].isin(left_hand_DB7)].index, inplace=True)
-###########################################################################
+# # # %% Loading and merging datasets
+# # Choose which window to use (so which dataframe to load)
 
-# DB3 (# for some reason subject 1 has done less exercise) -> [ 0,  4,  7,  8,  9, 10, 11, 12, 13]
-df_DB3 = pd.read_csv(f'/home/ricardo/DB3/Dataframe/dataframe_wnd_{wnd_len}_filt_20_500.csv',
-                     dtype={'sub': np.int8, 'label': np.int8, 'rep': np.int8, 'sample_index': np.uint32})
+# wnd_len = 200
 
-############ DROPPING LEFT HANDED SUBJECT
-left_hand_DB3 = [2, 5, 6]
-df_DB3.drop(df_DB3[df_DB3['sub'].isin(left_hand_DB3)].index, inplace=True)
-##############################################################################################
-# Setting ch9 and ch10 of subjects 6 and 7 to zero (not used and set as -1 for space problems)
-# df_DB3.loc[df_DB3['sub'] == 6, 'ch9'] = 0     # -> commented because I already dropped it as left-handed
-# df_DB3.loc[df_DB3['sub'] == 6, 'ch10'] = 0
-df_DB3.loc[df_DB3['sub'] == 7, 'ch9'] = 0
-df_DB3.loc[df_DB3['sub'] == 7, 'ch10'] = 0
+# # DB2            -> Left handed already deleted at creation
+# df_DB2 = pd.read_csv(f'/home/riccardo/EMG_data/DB2/Dataframe/dataframe_wnd_{wnd_len}.csv',
+#                      dtype={'sub': np.int8, 'label': np.int8, 'rep': np.int8, 'sample_index': np.uint32})
 
-# %% Merging dataset and reindexing samples and subjects
-# Changing sub in the order -> DB2 -> DB7 -> DB3
-df_DB7['sub'] = df_DB7['sub'] + np.max(df_DB2['sub'])
-df_DB3['sub'] = df_DB3['sub'] + np.max(df_DB7['sub'])
+# # DB7
+# df_DB7 = pd.read_csv(f'/home/riccardo/EMG_data/DB7/Dataframe/dataframe_wnd_{wnd_len}.csv',
+#                      dtype={'sub': np.int8, 'label': np.int8, 'rep': np.int8, 'sample_index': np.uint32})
+# ############ DROPPING LEFT HANDED ############################################
+# left_hand_DB7 = [1, 4, 5, 14]
+# df_DB7.drop(df_DB7[df_DB7['sub'].isin(left_hand_DB7)].index, inplace=True)
+# #############################################################################
 
-# Concatenate them
-df = pd.concat([df_DB2, df_DB7, df_DB3])
-if len(df_DB2) + len(df_DB3) + len(df_DB7) != len(df):
-    print('Something went wrong during concatenation!')
-# Free space
-del df_DB2, df_DB7, df_DB3
+# # DB3 (# for some reason subject 1 has done less exercise) -> [ 0,  4,  7,  8,  9, 10, 11, 12, 13]
+# df_DB3 = pd.read_csv(f'/home/riccardo/EMG_data/DB3/Dataframe/dataframe_wnd_{wnd_len}.csv',
+#                      dtype={'sub': np.int8, 'label': np.int8, 'rep': np.int8, 'sample_index': np.uint32})
 
-# Fixing the "sample_index" column that will be picked from dataloaders
-# calculate the maximum value for the new column
-n_samples = len(df) // wnd_len
+# ############ DROPPING LEFT HANDED ###########################################################
+# left_hand_DB3 = [2, 5, 6]
+# df_DB3.drop(df_DB3[df_DB3['sub'].isin(left_hand_DB3)].index, inplace=True)
+# ##############################################################################################
 
-# create an array of values that are repeated WND_LEN times
-samples_index = np.repeat(range(n_samples), wnd_len)
+# # Setting ch9 and ch10 of subjects 6 and 7 to zero (not used and set as -1 for space problems)
+# # df_DB3.loc[df_DB3['sub'] == 6, 'ch9'] = 0     # -> commented because I already dropped it as left-handed
+# # df_DB3.loc[df_DB3['sub'] == 6, 'ch10'] = 0
+# df_DB3.loc[df_DB3['sub'] == 7, 'ch9'] = 0
+# df_DB3.loc[df_DB3['sub'] == 7, 'ch10'] = 0
 
-# Reindexing samples
-df['sample_index'] = samples_index
+# # %% Merging dataset and reindexing samples and subjects
+# # Changing sub in the order -> DB2 -> DB7 -> DB3
+# df_DB7['sub'] = df_DB7['sub'] + np.max(df_DB2['sub'])
+# df_DB3['sub'] = df_DB3['sub'] + np.max(df_DB7['sub'])
 
-# Save dataframe
-df.to_csv(path_or_buf=f'/home/ricardo/DB2+DB7+DB3/Dataframe/dataframe_wnd_{wnd_len}.csv')
+# # Concatenate them
+# df = pd.concat([df_DB2, df_DB7, df_DB3])
+# if len(df_DB2) + len(df_DB3) + len(df_DB7) != len(df):
+#     print('Something went wrong during concatenation!')
+# # Free space
+# del df_DB2, df_DB7, df_DB3
+
+# # Fixing the "sample_index" column that will be picked from dataloaders
+# # calculate the maximum value for the new column
+# n_samples = len(df) // wnd_len
+
+# # create an array of values that are repeated WND_LEN times
+# samples_index = np.repeat(range(n_samples), wnd_len)
+
+# # Reindexing samples
+# df['sample_index'] = samples_index
+
+# # Save dataframe
+# df.to_csv(path_or_buf=f'/home/riccardo/EMG_data/DB2+DB7+DB3/Dataframe/dataframe_wnd_{wnd_len}.csv')
 
 #%% Script to be run from console for Grid_search
 if __name__ == '__main__':
@@ -88,8 +92,8 @@ if __name__ == '__main__':
     # %% Get params from prompt
     parser = argparse.ArgumentParser(description='Configurations to train models.')
 
-    parser.add_argument('-type', '--NORM_TYPE', help='Normalization_Type', type=str, default='z_score')
-    parser.add_argument('-mode', '--NORM_MODE', help='Normalization_Mode', type=str, default='channel')
+    parser.add_argument('-mode', '--NORM_MODE', help='Normalization_Mode',type=str, default='channel')
+    parser.add_argument('-norm', '--NORM_TYPE', help='Normalization_Type', type=str, default='z_score')
     parser.add_argument('-rec', '--RECT', help='Absolute value or normal', default='True', type=str)
     parser.add_argument('-wnd', '--WND_LEN', help='Windows length', type=int, default=300)
 
@@ -106,7 +110,7 @@ if __name__ == '__main__':
 
     # %% Load dataframe
     # Choose which window to use (so which dataframe to load)
-    database = '/home/ricardo/DB2+DB7+DB3/'
+    database = '/home/riccardo/EMG_data/DB2+DB7+DB3/'
     df = pd.read_csv(f'{database}/Dataframe/dataframe_wnd_{WND_LEN}.csv',
                      dtype={'sub': np.int8, 'label': np.int8, 'rep': np.int8, 'sample_index': np.uint32})
     # Making it lighter
@@ -163,16 +167,16 @@ if __name__ == '__main__':
             kernel_sizes[j][i][0] = (10*(i + 1) + kernels_gap[j])
         n_classes = 14
 
-    grid = {'net': {'N_multik': [16, 32, 64], 'N_Conv_conc': [64, 128, 256], 'N_SepConv': [64, 128, 256],
+    grid = {'net': {'N_multik': [32], 'N_Conv_conc': [128], 'N_SepConv': [128, 256],
                     'Kernel_multi_dim': [kernel_sizes[0], kernel_sizes[1], kernel_sizes[2]],
-                    'Kernel_Conv_conc': [1, 3],
-                    'act_func': [nn.ReLU(), nn.LeakyReLU(), nn.ELU(), nn.PReLU(), nn.GELU],
+                    'Kernel_Conv_conc': [1],
+                    'act_func': [nn.ReLU(), nn.LeakyReLU(), nn.ELU(), nn.PReLU()],
                     'Pool_Type': [nn.MaxPool2d, nn.AvgPool2d], 'wnd_len': [WND_LEN]
                     },
-            'learning': {'n_models': [50],
+            'learning': {'n_models': [15],
                          'num_epochs': [100],
-                         'lr': loguniform.rvs(1e-4, 1e-2, size=10),
-                         'batch_size': [128, 256, 512],
+                         'lr': [0.001, 0.0006], # loguniform.rvs(1e-4, 1e-2, size=10),
+                         'batch_size': [256, 512],
                          # 'folds': [5],    # In case of cross_validation, but here I use a manual validation set
                          'opt': ['Adam'],
                          'loss_fn': [nn.CrossEntropyLoss(reduction='mean').to(device)],
@@ -180,6 +184,14 @@ if __name__ == '__main__':
                          'num_workers': [4]
                          }
             }
+
+
+# %% Grid_search
+
+# Grid_search, divided by repetition (total 6): 4 for training one for validation one for test
+random_GS(grid=grid, train_set=train_set, valid_set=valid_set, test_set=test_set, cm_labels=exe_labels,
+          path_to_save=f'/home/riccardo/EMG_data/DB2+DB7+DB3/grid_search_{WND_LEN}_{NORM_TYPE}_{RECT}')
+
 
 
 # %% Grid_search
